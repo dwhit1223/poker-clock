990b983561aed06300e7a1a7373f901e6ed7745c
Derek Whitford <dwhit1223@gmail.com>
2026-02-24 12:17:48 -0600

    Deploy PRO to GitHub Pages via Actions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// IMPORTANT for GitHub Pages:
-// If your repo is https://github.com/<you>/<repo>, set base to "/<repo>/"
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE ?? "/poker-clock/",
